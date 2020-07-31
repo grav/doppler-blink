6f14bb03b27588f9a80c05064dffb1ee04ec61fe
Mikkel Gravgaard <mikkel@klokke.dk>
2020-07-31 19:47:54 +0200

    wip

--- top.v
+++ top.v
@@ -1,7 +1,9 @@
-module top(input clk_100mhz, output [3:1] led);
+module top(input clk_100mhz, output [3:1] aled, output [3:1] kled);
 
 reg [25:0] counter = 0;
-assign led = counter[25:23];
+assign aled = counter[25:23];
+assign kled = counter[25:23];
+
 always @(posedge clk_100mhz) counter <= counter + 1;
 
 endmodule     
